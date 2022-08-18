233aed9fc009d30997b6cf45cbc962e822850343
Dağhan Güven <moderator0180@gmail.com>
2022-08-18 01:18:17 -0400

    Add files via upload

---
+++ Morse/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Morse
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(18, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(69, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(49, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 70);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(227, 177);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Morse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Morse/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Morse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string morse = textBox1.Text;
+
+            for (int i = 0; i < morse.Length; i++)
+            {
+                PlaySound(morse[i].ToString());
+                Wait();
+            }
+        }
+
+        int shortt = 150;
+        int longg = 400;
+
+        private void PlaySound(string c)
+        {
+            c=c.ToLower();
+            if (c == " ")
+            {
+                Thread.Sleep(400);
+                return;
+            }
+            if (c == "a")
+            {
+                Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "b")
+            {
+                Beep(longg); Beep(shortt); Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "c")
+            {
+                Beep(longg); Beep(shortt); Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "d")
+            {
+                Beep(longg); Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "e")
+            {
+                Beep(shortt);
+                return;
+            }
+            if (c == "f")
+            {
+                Beep(shortt); Beep(shortt); Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "g")
+            {
+                Beep(longg); Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "h")
+            {
+                Beep(shortt); Beep(shortt); Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "i" || c == "ı")
+            {
+                Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "j")
+            {
+                Beep(shortt); Beep(longg); Beep(longg); Beep(longg);
+                return;
+            }
+            if (c == "k")
+            {
+                Beep(longg); Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "l")
+            {
+                Beep(shortt); Beep(longg); Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "m")
+            {
+                Beep(longg); Beep(longg);
+                return;
+            }
+            if (c == "n")
+            {
+                Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "o")
+            {
+                Beep(longg); Beep(longg); Beep(longg);
+                return;
+            }
+            if (c == "p")
+            {
+                Beep(shortt); Beep(longg); Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "q")
+            {
+                Beep(longg); Beep(longg); Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "r")
+            {
+                Beep(shortt); Beep(longg); Beep(shortt);
+                return;
+            }
+            if (c == "s")
+            {
+                Beep(shortt); Beep(shortt); Beep(shortt);
+                return;
+            }
+            if (c == "t")
+            {
+                Beep(longg);
+                return;
+            }
+            if (c == "u")
+            {
+                Beep(shortt); Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "v")
+            {
+                Beep(shortt); Beep(shortt); Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "w")
+            {
+                Beep(shortt); Beep(longg); Beep(longg);
+                return;
+            }
+            if (c == "x")
+            {
+                Beep(longg); Beep(shortt); Beep(shortt); Beep(longg);
+                return;
+            }
+            if (c == "y")
+            {
+                Beep(longg); Beep(shortt); Beep(longg); Beep(longg);
+                return;
+            }
+            if (c == "z")
+            {
+                Beep(longg); Beep(longg); Beep(shortt); Beep(shortt);
+                return;
+            }
+        }
+
+        private void Wait() => Thread.Sleep(300);
+
+        private void Beep(int len) => Console.Beep(757, len);
+    }
+}
